826a14cfdfb487959194864d059064ed15d0211c
Radu Solca <solca.radu@yahoo.com>
2016-05-24 18:55:52 +0300

    Add ratings to StoryList
    Also, added a new view to database scripts, update required.

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -7,7 +7,7 @@
 			$stList = $this->model('StoryList');
 
 			//get the newest 5 stories;
-			$stList->getStories(null, null, 5, 'st_date_added', null);
+			$stList->getStories(null, null, 5, 'DATE_ADDED', null);
 			$result = $stList->list;
 
 			$this->view('menu/index');

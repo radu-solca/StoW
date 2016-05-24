2f2a40ef18a92539fa73af4b410ecadcdd383ce7
Radu Solca <solca.radu@yahoo.com>
2016-05-24 15:35:20 +0300

    Separate menu view from home view

---
+++ app/views/menu/index.php
@@ -0,0 +1,38 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>StoW // Stories on the Web</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,400italic' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Amatic+SC:700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" media="screen and (max-width:500px)" href="assets/css/style_mobile.css" />
+
+	<script type="text/javascript" src="assets/js/mobile.js"></script>
+
+	<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+</head>
+<body>
+	<nav class="fullWidth">
+		<div class="navDeco"></div>
+		<div class="guide960">
+			<div id="navContent" class="flex centerV spaceBetween">
+				<div id="left" class="flex centerV flexStart">
+					<a href="" class="iconic home"></a>
+					<a href="" class="iconic book"></a>
+					<a href="" class="iconic upload"></a>
+				</div>
+				<div id="right" class="flex centerV flexEnd">
+					<a href="" class="iconic magnifying_glass"></a>
+					<a href="" class="iconic user"></a>
+				</div>
+			</div>
+		</div>
+	</nav>
+</body>
+</html>

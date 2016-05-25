c4350e2e388de0eb3ee72f36682ac3f9b737721a
Radu Solca <solca.radu@yahoo.com>
2016-05-25 19:14:28 +0300

    Add register.php + Register controller

---
+++ app/views/register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>StoW // Stories on the Web</title>
+</head>
+<body>
+	<form action="../views/login.php" method="post">
+		<div>
+			Username: <input type="text" name="username">
+		</div>
+		<div>
+			Password: <input type="password" name="password">
+		</div>
+		<div>
+			Email: <input type="text" name="email">
+		</div>
+		<div>
+			Name: <input type="text" name="name">
+		</div>
+		<div>
+			Surname: <input type="text" name="surname">
+		</div>
+	</form>
+</body>

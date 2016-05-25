9f56e545c1046071a2747bbd44de788ebde508ec
Radu Solca <solca.radu@yahoo.com>
2016-05-25 22:31:49 +0300

    Make register form retain data on validation failure

--- app/views/register.php
+++ app/views/register.php
@@ -44,22 +44,22 @@ if(!empty($_POST)){
 <body>
 	<form action="register" method="post">
 		<div>
-			Username: <input type="text" name="username">
+			Username: <input type="text" name="username" value="<?php echo @$_POST['username']; ?>">
 		</div>
 		<div>
-			Password: <input type="password" name="password">
+			Password: <input type="password" name="password" value="<?php echo @$_POST['password']; ?>">
 		</div>
 		<div>
-			Repeat password: <input type="password" name="repeat_password">
+			Repeat password: <input type="password" name="repeat_password" value="<?php echo @$_POST['repeat_password']; ?>">
 		</div>
 		<div>
-			Email: <input type="text" name="email">
+			Email: <input type="text" name="email" value="<?php echo @$_POST['email']; ?>">
 		</div>
 		<div>
-			Name: <input type="text" name="name">
+			Name: <input type="text" name="name" value="<?php echo @$_POST['name']; ?>">
 		</div>
 		<div>
-			Surname: <input type="text" name="surname">
+			Surname: <input type="text" name="surname" value="<?php echo @$_POST['surname']; ?>">
 		</div>
 		<input type="submit">
 	</form>

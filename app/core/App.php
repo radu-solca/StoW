9204af0e77f713b9811a4c4107ae1fe7aca296b9
Radu Solca <solca.radu@yahoo.com>
2016-05-27 13:20:33 +0300

    Change App::redirect to take the user to root/param instead of root/public/param

--- app/core/App.php
+++ app/core/App.php
@@ -54,7 +54,7 @@ class App{
 	public static function redirect($path){
 		$host  = $_SERVER['HTTP_HOST'];
 		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		header("Location: http://$host$uri/$path");
+		header("Location: http://$host$uri/../$path");
 		exit;
 	}
 }

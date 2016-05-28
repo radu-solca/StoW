3fb4efdd6f300d3029b443ed94735c4976028bb5
elena.chiosa <elena.chiosa@info.uaic.ro>
2016-05-28 14:23:17 +0300

    Delete ugly javascript for responsive menu

--- app/views/menu.php
+++ app/views/menu.php
@@ -2,7 +2,8 @@
 		<div class="navDeco fullWidth fixedTop flex center1 center2"></div>
 		<div class="guide960">
 			<div class="margin20">
-				<div id="navContent" class="flex center1 spaceBetween">
+
+				<div id="navContent" class="flex center1 spaceBetween" >
 					<div id="left" class="flex centerV flexStart">
 						<a href="" class="iconic home"></a>
 						<a href="" class="iconic book"></a>
@@ -10,9 +11,23 @@
 					</div>
 					<div id="right" class="flex centerV flexEnd">
 						<a href="" class="iconic magnifying_glass"></a>
-						<a href="" class="iconic user"></a>
+						<?php 
+						echo "<a href=\"#\" data-attr=\"Welcome, ";
+						echo isset($_SESSION['userData'])?$_SESSION['userData']['USERNAME']:"visitor";
+						echo "\" class=\"iconic user\"></a>";
+						?>
 					</div>
 				</div>
+
+				<div id="navContentMobile" class="flex center1 spaceBetween">
+						<a href="" class="iconic home"></a>
+						<a href="" class="iconic book"></a>
+						<a href="" class="iconic upload"></a>
+						<a href="" class="iconic magnifying_glass"></a>
+						<a href="" class="iconic user"></a>					
+				</div>
+
 			</div>
 		</div>
 	</nav>
+

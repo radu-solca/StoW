1f82bbc609b99473e462da8457795012fdd74074
elena.chiosa <elena.chiosa@info.uaic.ro>
2016-05-31 21:42:22 +0300

    Add upload (view) customisation

--- app/views/upload.php
+++ app/views/upload.php
@@ -1,48 +1,83 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<!-- 	<meta charset="UTF-8">
-	<title>StoW // Register</title>
+	<meta charset="UTF-8">
+	<title>StoW // Login</title>
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
 	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,400italic,700' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Amatic+SC:700' rel='stylesheet' type='text/css'>
 
-	<link rel="stylesheet" href="assets/css/style.css">
-	<link rel="stylesheet" media="screen and (max-width:500px)" href="assets/css/style_mobile.css" />
+	<link rel="stylesheet" href="<?php echo App::makeAbsolute("assets/css/style.css"); ?>">
+	<link rel="stylesheet" media="screen and (max-width:500px)" href="<?php echo App::makeAbsolute("assets/css/style_mobile.css"); ?>" />
 
-	<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico"> -->
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo App::makeAbsolute("assets/img/favicon.ico"); ?>">
+	
 </head>
-<body style="color:black;margin-top:4%">
+<body>
 
-<?php 
+	<?php 
 	require "../app/views/menu.php";
 	?>
 
+	<div class="flexWrap flex column">
 
-<form action="upload" method="post" enctype="multipart/form-data">
-    	Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload zip" name="submit">
-</form>
+		<div class="uploadForm fullWidth flex center1 center2 first">
+			<div class="guide960">
+				<div class="margin20 flex center2 topBottom40">
+					<div class="flex formContent center2 center1 column">
+						<h1 class="title">Upload</h1>
+						<p class="description">
+							Select .zip file to be uploaded.
+						</p> 
 
+						<form action="upload" method="post" enctype="multipart/form-data" class="flex column center1">
+							<div class="fileUpload">
+								<input type="file" name="fileToUpload" id="fileToUpload" accept=".zip">
+								<label id="fileToUploadLabel" for="file">
+									No file selected.
+								</label>
+
+							</div>
+							<input type="submit" value="Submit" name="submit">
+						</form>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<?php 
+		require "../app/views/footer.php";
+		?>
+
+	</div>
+
+
+	<script src="assets/js/global.js"></script>
+	<script src="assets/js/ajax.js"></script>
 	<script src="assets/js/nav.js"></script>
-<!-- 	<script src="assets/js/ajax.js"></script>
- -->
-	<!-- <script type="text/javascript">
+	<script src="assets/js/upload_filename.js"></script>
+
+	<!--
+	<script type="text/javascript">
 	function submitUpload(){
 		var file = encodeURIComponent(document.getElementById('fileToUpload').value);
 		var params = "fileToUpload="+file;
 		
 		//alert();
-	  	ajaxPost(	"upload", 
-					params, 
-					function(responseText){
-						alert(responseText);
-					});
+		ajaxPost(	"upload", 
+			params, 
+			function(responseText){
+				alert(responseText);
+			});
 
 	}
-	</scrip -->t>
-	
+	</script>
+-->
+
 </body>
+</html>
+
---
+++ public/assets/js/upload_filename.js
@@ -0,0 +1,15 @@
+var input = document.querySelector('#fileToUpload');
+
+var label = document.querySelector('#fileToUploadLabel'),
+    labelVal = label.innerHTML;
+
+input.addEventListener('change', function(file) {
+    var fileName = '';
+    if (this.files)
+        fileName = file.target.value.split('\\').pop();
+
+    if (fileName)
+        label.innerHTML = fileName;
+    else
+        label.innerHTML = labelVal;
+});

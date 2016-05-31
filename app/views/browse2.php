e9a522d6b8b32e46dab72a41cd5f0e6d6ef4161b
elena.chiosa <elena.chiosa@info.uaic.ro>
2016-05-31 13:31:38 +0300

    Add categories (hardcoded) to browse2

--- app/views/browse2.php
+++ app/views/browse2.php
@@ -26,21 +26,60 @@
 
 		<div class="browse fullWidth flex center1 center2 first">
 			<div class="guide960">
-				<div class="margin20">
-					<div class="browseContent margin20 flex center1 center2 row topBottom40">
+					<div class="browseContent margin20 flex flexStart center2 row topBottom40">
 
 						<section id="filterPanel">
-							ayylamo		
+							<p>Filter stories</p>
+
+							<div class="ageGroup">
+								<p>Age group</p>
+								<ul>
+									<!-- DUMMIES, TREBUIE DIN BAZA DE DATE-->
+									<li>0-3</li>
+									<li>3-5</li>
+									<li>5-7</li>
+									<li>7-12</li>
+									<li>12-15</li>
+									<li>15-18</li>
+									<li>18+</li>
+								</ul>
+							</div>
+
+							<div class="genre">
+								<p>Genre</p>
+								<ul>
+									<!-- DUMMIES, TREBUIE DIN BAZA DE DATE-->
+									<li>drama</li>
+									<li>classic</li>
+									<li>comic</li>
+									<li>crime</li>
+									<li>fable</li>
+									<li>fairy tale</li>
+									<li>biography</li>
+									<li>autobiography</li>
+									<li>memoir</li>
+									<li>fantasy</li>
+									<li>folklore</li>
+									<li>historical</li>
+									<li>poetry</li>
+									<li>horror</li>
+									<li>legend</li>
+									<li>mystery</li>
+									<li>mythology</li>
+								</ul>
+							</div>
+
 						</section>	
 
 						<hr class="vertical">	
 
 						<section id="filteredResults">	
-							ayayayac
+							<p>Filtered results</p>
+
+							
 						</section>	
 					</div>
 
-				</div>
 			</div>
 		</div>
 

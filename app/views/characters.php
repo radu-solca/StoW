73afa2b68f128d01128b4de4586306ec891c4238
elena.chiosa <elena.chiosa@info.uaic.ro>
2016-06-06 23:46:00 +0300

    Add half-assed characters page :)

--- app/views/characters.php
+++ app/views/characters.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>StoW // Characters</title>
+
+	<title>StoW // <?php print_r( $data['json']->story->meta->title);?>: Characters</title>
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
@@ -10,6 +11,8 @@
 	<link href='https://fonts.googleapis.com/css?family=Amatic+SC:700' rel='stylesheet' type='text/css'>
 
 	<link rel="stylesheet" href="<?php echo App::makeAbsolute("assets/css/style.css"); ?>">
+	<link rel="stylesheet" href="<?php echo App::makeAbsolute("assets/css/iconic_fill.css"); ?>">
+	<link rel="stylesheet" href="<?php echo App::makeAbsolute("assets/css/iconic_stroke.css"); ?>">
 	<link rel="stylesheet" media="screen and (max-width:500px)" href="<?php echo App::makeAbsolute("assets/css/style_mobile.css"); ?>" />
 
 	<link rel="shortcut icon" type="image/x-icon" href="<?php echo App::makeAbsolute("assets/img/favicon.ico"); ?>">
@@ -17,32 +20,78 @@
 </head>
 <body>
 
+
 	<?php 
 	require "../app/views/menu.php";
 	?>
 
-	<?php 
-		echo '<h1>', $data['json']->story->meta->title, '</h1>';
-	?>
-		<ul>
-	<?php
-		foreach($data['json']->story->characters as $character){
-			$src = App::makeAbsolute($data['path'].'/'.$character->img);
-	?>
-			<li>
-				<img src="<?php echo $src; ?>"/>
-				<span><?php echo $character->name; ?></span>
-				<span><?php echo $character->description; ?></span>
-			</li>
-	<?php
-		}//end foreach
-	?>
-		</ul>
 
-	<?php 
-	require "../app/views/footer.php";
-	?>
 
+	<div class="flexWrap flex column">
+
+		<div class="storyById fullWidth flex center1 center2 first">
+			<div class="guide960">
+				<div id="story" class="margin20 storyByIdContent flex center1 center2 column topBottom40">
+
+					<div id="storyDetails" class="flex row overlay">
+						<section id="left">
+							<h1>
+								<?php
+								if($data){
+									print_r($data['json']->story->meta->title);
+									echo " - Characters";
+								}else{
+									print_r("No title");
+								}
+								?>
+
+							</h1>
+							
+							<div id="rating"></div>
+
+						</section>
+					</div>
+
+					<div id="storyAndProgress" class="flex column storyCharacters">
+						
+						<div id="bothStoryPages" class="flex column">
+						
+							<?php
+								foreach($data['json']->story->characters as $character){
+									$src = App::makeAbsolute($data['path'].'/'.$character->img);
+							?>
+								
+									<img src="<?php echo $src; ?>"/>
+									<h1><?php echo $character->name; ?></h1>
+									<span><?php echo $character->description; ?></span>
+									<hr>
+							<?php
+							}//end foreach
+							?>
+
+						</div>
+
+
+					</div>
+
+				</div>
+
+
+			</div>
+		</div>
+
+
+		<?php 
+		require "../app/views/footer.php";
+		?>
+
+	</div>
+
+	<script src="<?php echo App::makeAbsolute("assets/js/ajax.js"); ?>"></script>
 	<script src="<?php echo App::makeAbsolute("assets/js/global.js"); ?>"></script>
+	<script>
+		assignColors("storyByIdContent");
+	</script>
+
 </body>
 </html>
